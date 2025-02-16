108fee2c6f9db68cefdfd7a07175f761791ab060
ratishjain <ratishjain6@gmail.com>
2025-02-16 12:37:56 +0530

    removed bottom CTA

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,19 +7,6 @@ export default function Footer() {
   const router = useRouter();
   return (
     <footer className="w-full">
-      {/* Contact Section */}
-      <div className="bg-black dark:bg-[#1A1A1A] py-8 sm:py-12 md:py-16">
-        <h2 className="text-[#FF6B00] text-center font-sans text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold  px-4">
-          LET&apos;S CHAT 🚀
-        </h2>
-        <PulsatingButton
-          onClick={() => router.push("https://calendly.com/ratishjain6/30min")}
-          className="font-[700] dark:text-white mt-6 mx-auto"
-        >
-          Book a call
-        </PulsatingButton>
-      </div>
-
       {/* Footer Content */}
       <div className="bg-[#FDF6EC] dark:bg-[#2A2A2A] py-8 pb-32 px-4 sm:px-6 lg:px-8 text-gray-800 dark:text-gray-200">
         <div className="container mx-auto">
