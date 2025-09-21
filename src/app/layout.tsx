a20b1ad121905a17312e5211402726361d66a912
ratish jain <ratishjain6@gmail.com>
2025-09-21 22:00:42 +0530

    removed unecessary imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Mulish, Poppins } from "@next/font/google";
+import { Poppins } from "@next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Navbar from "@/components/Navbar";
 import Script from "next/script";
-const mulish = Mulish({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
 
 const poppins = Poppins({
   subsets: ["latin"],
